e69a3419e6b8bc6c01fcc29cc09f3fac41b2873a
Ataur Rahman <rahman.sharif39n@gmail.com>
2023-12-14 14:21:47 +0600

    pencil canvas configured

--- src/components/whiteboard/index.tsx
+++ src/components/whiteboard/index.tsx
@@ -1,11 +1,90 @@
-import { useState } from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import rough from "roughjs";
+import { RoughCanvas } from 'roughjs/bin/canvas';
+
 function Whiteboard() {
+
+    // canvas reference state
+    const canvasRef = useRef(null);
+    // canvas context reference state
+    const ctxRef = useRef(null);
 
     // draw tool type - pencil, line , rectangle
     const [drawType, setDrawType] = useState('pencil')
+    // draw color
+    const [drawColor, setDrawColor] = useState('black')
+    const [allowDrawing, setAllowDrawing] = useState(false)
 
-    console.log({ drawType });
+    const [elements, setElements] = useState([]) as any[]
+
+    useEffect(() => {
+        const canvas = canvasRef.current as any
+        const ctx = canvas?.getContext('2d')
+
+        ctxRef.current = ctx
+    }, [])
+
+    useLayoutEffect(() => {
+        const roughCanvas = canvasRef.current !== null && rough.canvas(canvasRef.current)
+
+        elements.forEach((element: any) => {
+            if (element.type === "pencil") {
+                (roughCanvas as RoughCanvas).linearPath(element.path)
+            }
+
+        })
+
+    }, [elements])
+
+    const handleMouseDown = (e: any) => {
+        // console.log(e);
+        setAllowDrawing(true)
+        const { offsetX, offsetY } = e.nativeEvent
+        allowDrawing && console.log(offsetX, offsetY);
+        if (drawType === "pencil") {
+            setElements((previousElements: any) => [
+                ...previousElements,
+                {
+                    type: 'pencil',
+                    offsetX,
+                    offsetY,
+                    path: [[offsetX, offsetY]],
+                    stroke: 'black'
+                }
+            ])
+        }
+
+    }
+    const handleMouseMove = (e: any) => {
+        const { offsetX, offsetY } = e.nativeEvent
+        allowDrawing && console.log(offsetX, offsetY);
+        if (allowDrawing) {
+            if (drawType === "pencil") {
+                const { path } = elements[elements.length - 1]
+                const newPath = [...path, [offsetX, offsetY]]
+
+                setElements((previousElements: any[]) => previousElements.map((element: any, index: number) => {
+                    if (index === elements.length - 1) {
+                        return {
+                            ...element,
+                            path: newPath
+                        }
+                    } else {
+                        return element
+                    }
+                }))
+            }
+        }
+
+    }
+    const handleMouseUp = (e: any) => {
+        setAllowDrawing(false)
+        const { offsetX, offsetY } = e.nativeEvent
+        allowDrawing && console.log(offsetX, offsetY);
+
+
+    }
 
 
     return (
@@ -27,11 +106,15 @@ function Whiteboard() {
                     <Button variant="outline-danger"> Clear </Button>
                 </div>
             </div>
-            <Card border="primary h-100 m-5">
-                <Card.Body id='onlyCanvas'>
-                    <p>This is card body</p>
-                </Card.Body>
-            </Card>
+            <div className='h-100 p-3'>
+                <canvas
+                    ref={canvasRef}
+                    className='h-100 w-100 border border-black'
+                    onMouseDown={(e) => handleMouseDown(e)}
+                    onMouseUp={(e) => handleMouseUp(e)}
+                    onMouseMove={(e) => handleMouseMove(e)}
+                />
+            </div>
         </div>
     )
 }
